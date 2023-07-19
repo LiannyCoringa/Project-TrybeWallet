5734dfbc6ae74729b54a75166bcf86c0f4f019de
Lianny Coringa <lianny_costa@hotmail.com>
2023-07-19 14:00:11 -0300

    Requisito 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,48 @@
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { RootState } from '../types';
+
 function Table() {
-  return <div>Table</div>;
+  const rootStateExpenses = useSelector((state: RootState) => state.wallet.expenses);
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <table>
+      <tr>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </tr>
+      <tbody>
+        {
+        rootStateExpenses.map((objeto: any) => (
+          <tr key={ objeto.id }>
+            <td>{ objeto.description }</td>
+            <td>{ objeto.tag }</td>
+            <td>{ objeto.method }</td>
+            <td>{ Number(objeto.value).toFixed(2) }</td>
+            <td>{ objeto.exchangeRates[objeto.currency].name }</td>
+            <td>{ Number(objeto.exchangeRates[objeto.currency].ask).toFixed(2) }</td>
+            <td>
+              { (objeto.exchangeRates[objeto.currency].ask * objeto.value).toFixed(2) }
+            </td>
+            <td>Real</td>
+            <td>Editar</td>
+          </tr>
+        ))
+      }
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/types.ts
+++ src/types.ts
@@ -23,3 +23,110 @@ export type Despesas = {
   method: string,
   tag: string,
 };
+
+export type WalletState = {
+  id: number,
+  value: string,
+  description: string,
+  currency: string,
+  method: string,
+  tag: string,
+  exchangeRates: {
+    USD: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    USDT: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    CAD: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    GBP: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    ARS: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    BTC: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    LTC: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    EUR: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    JPY: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    CHF: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    AUD: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    CNY: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    ILS: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    ETH: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    XRP: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+    DOGE: {
+      code: string,
+      codein: string,
+      name: string,
+      ask: string,
+    },
+  };
+};
