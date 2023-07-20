3d63debadeec0e18ebfa090dad2e6a8cf4f3023e
Lianny Coringa <lianny_costa@hotmail.com>
2023-07-20 15:16:47 -0300

    Testando as partes fixas

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -38,7 +38,39 @@ describe('Testa o Header do Forms', () => {
     const moeda = screen.getByTestId('header-currency-field');
 
     expect(email).toBeInTheDocument();
-    expect(total).toBe(0.00);
-    expect(moeda).toBe('BRL');
-  })
+    expect(total).toBeInTheDocument();
+    expect(moeda).toBeInTheDocument();
+    expect(total).toHaveTextContent('0.00');
+    expect(moeda).toHaveTextContent('BRL');
+  });
+  test('Ao acessar a rota "/carteira", o forms renderiza', () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
+
+    const inputValue = screen.getByTestId('value-input');
+    const inputDescription = screen.getByTestId('description-input');
+    const selectCurrency = screen.getByTestId('currency-input');
+    const selectMethod = screen.getByTestId('method-input');
+    const selectTag = screen.getByTestId('tag-input');
+    const buttonAdd = screen.getByRole('button', { name: 'Adicionar despesa' });
+
+    expect(inputValue).toBeInTheDocument();
+    expect(inputDescription).toBeInTheDocument();
+    expect(selectCurrency).toBeInTheDocument();
+    expect(selectMethod).toBeInTheDocument();
+    expect(selectTag).toBeInTheDocument();
+    expect(buttonAdd).toBeInTheDocument();
+  });
+  test('Ao acessar a rota "/carteira", a tabela renderiza', () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/carteira'] });
+
+    expect(screen.getByText('Descrição')).toBeInTheDocument();
+    expect(screen.getByText('Tag')).toBeInTheDocument();
+    expect(screen.getByText('Método de pagamento')).toBeInTheDocument();
+    expect(screen.getByText('Valor')).toBeInTheDocument();
+    expect(screen.getByText('Moeda')).toBeInTheDocument();
+    expect(screen.getByText('Câmbio utilizado')).toBeInTheDocument();
+    expect(screen.getByText('Valor convertido')).toBeInTheDocument();
+    expect(screen.getByText('Moeda de conversão')).toBeInTheDocument();
+    expect(screen.getByText('Editar/Excluir')).toBeInTheDocument();
+  });
 });
